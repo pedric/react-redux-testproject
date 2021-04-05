4e23fc1c754c2e0892a998f46bbce99e1eae269b
Fredrik <epost.larsson@gmail.com>
2021-04-05 23:11:38 +0200

    thunks, ad initial state etc

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,74 +1,65 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import propTypes from "prop-types";
 import { bindActionCreators } from "redux";
+import CourseList from "./CourseList";
 
 class CoursesPage extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     course: {
-  //       title: "",
-  //     },
-  //   };
-  // }
-
-  // handleChange = (e) => {
-  //   const course = { ...this.state.course, title: e.target.value };
-  //   this.setState({ course });
-  // };
-
-  // handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   this.props.actions.createCourse(this.state.course);
-  //   this.setState({ course: { title: "" } });
-  // };
-
   componentDidMount = () => {
-    this.props.actions.loadCourses().catch((error) => {
-      console.log("failed to load Courses,", error);
-    });
+    const { courses, authors, actions } = this.props;
+    if (courses.length === 0) {
+      actions.loadCourses().catch((error) => {
+        console.log("failed to load courses,", error);
+      });
+    }
+
+    if (authors.length === 0) {
+      actions.loadAuthors().catch((error) => {
+        console.log("failed to load authors,", error);
+      });
+    }
   };
 
   render() {
     return (
       <>
-        {/* <form onSubmit={this.handleSubmit}>
-          <h2>Courses</h2>
-          <h3>Add course</h3>
-          <input
-            type='text'
-            onChange={this.handleChange}
-            value={this.state.course.title}
-          />
-          <input type='submit' value='Save' />
-        </form> */}
         <h2>Courses</h2>
-        <div>
-          {this.props.courses.map((course, idx) => (
-            <div key={`${course.title}_${idx}`}>{course.title}</div>
-          ))}
-        </div>
+        <CourseList courses={this.props.courses} />
       </>
     );
   }
 }
 
 CoursesPage.propTypes = {
+  authors: propTypes.array.isRequired,
   courses: propTypes.array.isRequired,
   actions: propTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => {
   return {
-    courses: state.courses,
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map((course) => {
+            return {
+              ...course,
+              authorName: state.authors.find((a) => a.id === course.authorId)
+                .name,
+            };
+          }),
+    authors: state.authors,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    actions: bindActionCreators(courseActions, dispatch),
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+    },
   };
 };
 
---
+++ src/redux/reducers/initialState.js
@@ -0,0 +1,6 @@
+const state {
+  courses: [],
+  authors: []
+};
+
+export default state;
